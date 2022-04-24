ed0f7a22ac09abfd28d883164a6beb34fab40f78
Francesco <f.vezzi@studenti.unipi.it>
2022-04-24 12:01:36 +0200

    using old names for observation spaces

---
+++ quadruped_spring/manual_jumping_with_springs.py
@@ -0,0 +1,254 @@
+import gym
+import numpy as np
+
+from env.quadruped_gym_env import QuadrupedGymEnv
+from utils.evaluate_metric import EvaluateMetricJumpOnPlace
+from utils.monitor_state import MonitorState
+
+
+class JumpingStateMachine(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        if self.env._isRLGymInterface:
+            raise ValueError("disable RLGymInterface in env_configs")
+        if self.env._motor_control_mode != "TORQUE":
+            raise ValueError("motor control mode should be TORQUE")
+        self._settling_duration_steps = 1000
+        self._couching_duration_steps = 4000
+        assert self._couching_duration_steps >= 1000, "couching duration steps number should be >= 1000"
+        self._states = {"settling": 0, "couching": 1, "jumping_ground": 2, "jumping_air": 3, "landing": 4}
+        self._state = self._states["settling"]
+        self._flying_up_counter = 0
+        self._actions = {
+            0: self.settling_action,
+            1: self.couching_action,
+            2: self.jumping_explosive_action,
+            3: self.jumping_flying_action,
+            4: self.jumping_landing_action,
+        }
+        self._total_sim_steps = 9000
+        self.max_height = 0.0
+        self._step_counter = 0
+
+        self._time_step = self.env._time_step
+        self._robot_config = self.env._robot_config
+        self._enable_springs = self.env._enable_springs
+        self._jump_end = False
+        self._first_take_off = False
+
+    def compute_action(self):
+        return self._actions[self._state]()
+
+    def compensate_spring(self):
+        spring_action = self.env.robot._spring_torque
+        return -np.array(spring_action)        
+
+    def update_state(self):
+        if self._step_counter <= self._settling_duration_steps:
+            actual_state = self._states["settling"]
+        elif self._step_counter <= self._settling_duration_steps + self._couching_duration_steps:
+            actual_state = self._states["couching"]
+        else:
+            if not self._jump_end:
+                if not self.is_flying():
+                    actual_state = self._states["jumping_ground"]
+                else:
+                    if not self.flight_time_gone():
+                        actual_state = self._states["jumping_air"]
+                    else:
+                        self._jump_end = True
+            if self._jump_end:
+                actual_state = self._states["landing"]
+
+        self.max_height = max(self.max_height, self.env.robot.GetBasePosition()[2])
+        if self._state != actual_state:
+            print('********************')
+            print(f'{self._state} -> {actual_state}')
+            print(f'joint config is: {self.env.robot.GetMotorAngles()}')
+            print(f'sim time is: {self.env.get_sim_time()}')
+            print('********************')
+        self._state = actual_state
+
+    def flight_time_gone(self):
+        if not self._first_take_off:
+            self._take_off_time = self.env.get_sim_time()
+            self.vz = self.base_velocity()[2]
+            print(self.vz)
+            self._flight_time = self.vz / 9.81
+            self._first_take_off = True
+        self._flight_timer = self.env.get_sim_time() - self._take_off_time
+        return self._flight_timer >= self._flight_time
+    
+    def base_velocity(self):
+        q_dot = self.env.robot.GetMotorVelocities()
+        _, _, _, feet_contact = self.env.robot.GetContactInfo()
+        base_vel = np.zeros(3)
+        count = 1
+        for in_contact, id in zip(feet_contact, list(range(4))):
+            if in_contact:
+                Jac, _ = self.env.robot.ComputeJacobianAndPosition(id)
+                vel = - Jac @ q_dot[3 * id : 3 * (id + 1)]
+                base_vel = base_vel + (vel - base_vel) / count
+                count += 1
+        return base_vel
+
+    def settling_action(self):
+        if self.env._enable_springs:
+            config_des = np.array(self.env._robot_config.SPRINGS_REST_ANGLE * 4)
+            config_init = self.env._robot_config.INIT_MOTOR_ANGLES
+            config_ref = self.generate_ramp(
+                self._step_counter, 0, self._settling_duration_steps - 200, config_init, config_des
+            )
+            action = self.angle_ref_to_command(config_ref)
+        else:
+            config_des = self.env._robot_config.INIT_MOTOR_ANGLES
+            action = self.angle_ref_to_command(config_des)
+        return action
+
+    def couching_action(self):
+        max_torque = 35.55 * 0.9
+        min_torque = 0
+        i = self._step_counter
+        i_min = self._settling_duration_steps
+        i_max = i_min + self._couching_duration_steps - 500
+        torque_thigh = self.generate_ramp(i, i_min, i_max, 0, 16)
+        torque_calf = self.generate_ramp(i, i_min, i_max, min_torque, max_torque)
+        torques = np.array([0, torque_thigh, -torque_calf] * 4)
+        return torques
+
+    def jumping_explosive_action(self):
+        coeff = 1.0
+        f_rear = 190
+        f_front = coeff * f_rear
+        jump_command = np.full(12, 0)
+        for i in range(4):
+            if i < 2:
+                f = f_front
+            else:
+                f = f_rear
+            jump_command[3 * i : 3 * (i + 1)] = self.map_force_to_tau([0, 0, -f], i)
+            jump_command[3 * i] = 0
+        # print(jump_command)
+        return jump_command
+
+    def jumping_flying_action(self):
+        action = np.full(12, 0)
+        return action
+
+    def jumping_landing_action(self):
+        config_des = np.array(self.env._robot_config.SPRINGS_REST_ANGLE * 4)
+        config_des = np.array(self.env._robot_config.INIT_MOTOR_ANGLES)
+        config_des += np.array([0, -0.2, -0.2] * 2 + [0, 0, 0] * 2)
+        q = self.robot.GetMotorAngles()
+        dq = self.robot.GetMotorVelocities()
+        compensate_springs = np.full(12, 0)
+        if self.env._enable_springs:
+            kp = 20
+            kd = 10.0
+            compensate_springs = self.compensate_spring()
+        else:
+            kp = 55
+            kd = 0.8
+        torque = -kp * (q - config_des) - kd * dq
+        # action = self.angle_ref_to_command(config_des)
+        action = torque  # + compensate_springs
+        return action
+
+    def is_flying(self):
+        _, _, _, feetInContactBool = self.env.robot.GetContactInfo()
+        return 1 - np.all(feetInContactBool)
+
+    def is_landing(self):
+        if self._flying_up_counter >= 20:
+            return True
+        else:
+            self._flying_up_counter += 1
+            return False
+
+    def generate_ramp(self, i, i_min, i_max, u_min, u_max) -> float:
+        if i < i_min:
+            return u_min
+        elif i > i_max:
+            return u_max
+        else:
+            return u_min + (u_max - u_min) * (i - i_min) / (i_max - i_min)
+
+    def angle_ref_to_command(self, angles_ref):
+        q = self.robot.GetMotorAngles()
+        dq = self.robot.GetMotorVelocities()
+        if self.env._enable_springs:
+            kp = 70
+            kd = 0.8
+        else:
+            kp = 55
+            kd = 0.8
+        torque = -kp * (q - angles_ref) - kd * dq
+        return torque
+
+    def height_to_theta_des(self, h):
+        l = self.env._robot_config.THIGH_LINK_LENGTH
+        theta_thigh = np.arccos(h / (2 * l))
+        theta_des = np.array([0, theta_thigh, -2 * theta_thigh] * 4)
+        return theta_des
+
+    def map_force_to_tau(self, F_foot, i):
+        J, _ = self.env.robot.ComputeJacobianAndPosition(i)
+        tau = J.T @ F_foot
+        return tau
+
+    def step(self, action):
+
+        obs, reward, done, infos = self.env.step(action)
+        self._step_counter += 1
+
+        self.update_state()
+
+        return obs, reward, done, infos
+
+    def render(self, mode="rgb_array", **kwargs):
+        return self.env.render(mode, **kwargs)
+
+    def reset(self):
+        obs = self.env.reset()
+        return obs
+
+    def close(self):
+        self.env.close()
+
+
+def build_env():
+    env_config = {}
+    env_config["enable_springs"] = True
+    env_config["render"] = False
+    env_config["on_rack"] = False
+    env_config["enable_joint_velocity_estimate"] = False
+    env_config["isRLGymInterface"] = False
+    env_config["robot_model"] = "GO1"
+    env_config["motor_control_mode"] = "TORQUE"
+    env_config["action_repeat"] = 1
+    env_config["record_video"] = False
+    env_config["action_space_mode"] = "DEFAULT"
+    env_config["task_env"] = "JUMPING_ON_PLACE_ABS_HEIGHT_TASK"
+    env_config["adapt_spring_parameters"] = False
+
+    env = QuadrupedGymEnv(**env_config)
+    return env
+
+
+if __name__ == "__main__":
+
+    env = build_env()
+    env = JumpingStateMachine(env)
+    sim_steps = env._total_sim_steps + 3000
+
+    # env = MonitorState(env=env, path="logs/plots/manual_jumping", rec_length=sim_steps)
+    env = EvaluateMetricJumpOnPlace(env)
+    done = False
+    while not done:
+        action = env.compute_action()
+        obs, reward, done, info = env.step(action)
+        # print(env.robot.GetMotorVelocities()-env.get_joint_velocity_estimation())
+    # env.release_plots()
+    env.print_metric()
+    env.close()
+    print("end")
--- quadruped_spring/utils/evaluate_metric.py
+++ quadruped_spring/utils/evaluate_metric.py
@@ -1,12 +1,34 @@
 import gym
 import numpy as np
-from stable_baselines3.common.vec_env.base_vec_env import VecEnvWrapper
 
+# TODO:
+# Create Tuple with power, height_min, height_max, metric. Then print that one.
+
+class MetricInfo():
+    def __init__(self, metric = 0.0, power = 0.0, height_max = 0.0, height_min = 0.0):
+        self.metric_value = 0.0
+        self.power_max = 0.0
+        self.height_max = 0.0
+        self.height_min = 0.0
+    
+    def get_values(self):
+        return (self.metric_value, self.power_max, self.height_max, self.height_min)
+        
+    def best_metrics(a, b):
+        if a.metric_value >= b.metric_value:
+            return a
+        else:
+            return b
+        
 
 class EvaluateMetricJumpOnPlace(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
         self.init_metric()
+        self._sep = 20
+        self.jump_metric_old = MetricInfo()
+        # self.jump_metric_old = 0.0
+        self.height_max_old = 0.0
         self.flag_first = True
 
     def compute_max_power(self):
@@ -21,41 +43,88 @@ class EvaluateMetricJumpOnPlace(gym.Wrapper):
         dy = y - self.y_pos
         return np.sqrt(dx**2 + dy**2)
 
+    def update_metrics(self):
+        self.jump_metric_old = MetricInfo.best_metrics(self.jump_metric_old, self.jump_metric)
+        self.init_metric()
+
     def init_metric(self):
         self.x_pos, self.y_pos, self.height = self.env.robot.GetBasePosition()
         self.roll, _, self.yaw = abs(self.env.robot.GetBaseOrientationRollPitchYaw())
-        self.power = 0
+        self.power = 0.0
         self.penalization_invalid_contact = 0
+        self._landed = False
+        self._taking_off = False
+        self.jump_metric = MetricInfo(height_max=self.height, height_min=self.height)
+        self._init_height = self.height
+        self.bounce_counter = 0
+        self.all_feet_in_contact = True
 
     def eval_metric(self):
-        _, numInvalidContacts, _, _ = self.env.robot.GetContactInfo()
+        _, numInvalidContacts, _, feet_in_contact = self.env.robot.GetContactInfo()
         if numInvalidContacts > 0:
             self.penalization_invalid_contact = -10
-        self.power = max(self.power, self.compute_max_power())
+        self.power = max(self.jump_metric.power_max, self.compute_max_power())
         roll, _, yaw = self.env.robot.GetBaseOrientationRollPitchYaw()
         _, _, height = self.env.robot.GetBasePosition()
         self.roll = max(self.roll, abs(roll))
         self.yaw = max(self.yaw, abs(yaw))
-        self.height = max(self.height, abs(height))
+        self.jump_metric.height_max = max(self.jump_metric.height_max, abs(height))
+        self.jump_metric.height_min = min(self.jump_metric.height_min, abs(height))
+        if np.all(1 - np.array([feet_in_contact])):
+            self._taking_off = True
+            self._landed = False
+            if self.all_feet_in_contact:
+                self.bounce_counter += 1
+                self.all_feet_in_contact = False
+        if self._taking_off and np.all(np.array([feet_in_contact])):
+            self._landed = True
+            self._taking_off = False
+            self.all_feet_in_contact = True
+
+        self.get_metric()
+
+    def print_first_line_table(self):
+        s1 = "ID"
+        s2 = "METRIC"
+        s3 = "MAX_POWER [w]"
+        s4 = "MAX_HEIGHT [m]"
+        s5 = "MIN_HEIGHT [m]"
+        sep = self._sep
+        columns = [s1, s2, s3, s4, s5]
+        first_line = ""
+        # print('*' * (sep * len(columns) + 4))
+        for c in columns:
+            first_line += c + " " * (sep - len(c))
+        print(first_line)
+        return first_line
+
+    def fill_line(self, id):
+        metric, power, height_max, height_min = self.get_metric().get_values()
+        columns = [id, f"{metric:.3f}", f"{power:.3f}", f"{height_max:.3f}", f"{height_min:.3f}"]
+        line = ""
+        for c in columns:
+            line += str(c) + " " * (self._sep - len(str(c)))
+        print(line)
+        return line
 
     def get_metric(self):
-        rew_dist = 0.2 * np.exp(-self.compute_forward_distance() ** 2 / 0.1)
-        rew_roll = 0.1 * np.exp(-self.roll**2 / 0.1)
-        rew_yaw = 0.1 * np.exp(-self.yaw**2 / 0.1)
-        max_height = self.height
+        metric = 0
         max_power = self.power
+        if self._landed and abs(max_power) >= 0.01:
+            max_height_rel = max(self.jump_metric.height_max - self._init_height, 0)
+            rew_dist = 1 / 3 * max_height_rel * np.exp(-self.compute_forward_distance() ** 2 / 0.1)
+            rew_roll = 1 / 3 * max_height_rel * np.exp(-self.roll**2 / 0.1)
+            rew_yaw = 1 / 3 * max_height_rel * np.exp(-self.yaw**2 / 0.1)
 
-        if abs(max_power) >= 0.01:
-            metric = rew_dist + rew_roll + rew_yaw + max_height * 1000 / (2 * max_power)
-        else:
-            metric = 0
-        metric += self.penalization_invalid_contact
-
-        return max(-1, metric)
+            metric = rew_dist + rew_roll + rew_yaw + max_height_rel * 1000 / (2 * max_power)
+            metric += self.penalization_invalid_contact
+            metric -= max(self.bounce_counter - 1, 0) * 0.2
+        self.jump_metric.metric_value = max(self.jump_metric.metric_value, metric)
+        return MetricInfo.best_metrics(self.jump_metric, self.jump_metric_old)
 
     def print_metric(self):
-        print(f"the jump (on place) metric performance amounts to: {self.get_metric():.3f}")
-        print(f"the maximum reached height amounts to: {self.height:.3f}")
+        print(f"the jump (on place) metric performance amounts to: {self.get_metric().metric_value:.3f}")
+        print(f"the maximum reached height amounts to: {self.get_metric().height_max:.3f}")
 
     def step(self, action):
 
@@ -65,6 +134,8 @@ class EvaluateMetricJumpOnPlace(gym.Wrapper):
 
         obs, reward, done, infos = self.env.step(action)
         self.eval_metric()
+        if done:
+            self.update_metrics()
 
         return obs, reward, done, infos
 
@@ -77,85 +148,3 @@ class EvaluateMetricJumpOnPlace(gym.Wrapper):
 
     def close(self):
         self.env.close()
-
-
-######################################################################
-
-
-class EvaluateMetricJumpOnPlaceVecEnv(VecEnvWrapper):
-    def __init__(self, venv):
-        super().__init__(venv)
-        self.env = self.venv
-        self.init_metric()
-        self.flag_first = True
-
-    def compute_max_power(self):
-        tau = abs(self.env.robot.GetMotorTorques())
-        vel = abs(self.env.robot.GetMotorVelocities())
-
-        return max(*(tau * vel))
-
-    def compute_forward_distance(self):
-        x, y, _ = self.env.robot.GetBasePosition()
-        dx = x - self.x_pos
-        dy = y - self.y_pos
-        return np.sqrt(dx**2 + dy**2)
-
-    def init_metric(self):
-        self.x_pos, self.y_pos, self.height = self.env.robot.GetBasePosition()
-        self.roll, _, self.yaw = abs(self.env.robot.GetBaseOrientationRollPitchYaw())
-        self.power = 0
-        self.penalization_invalid_contact = 0
-
-    def eval_metric(self):
-        _, numInvalidContacts, _, _ = self.env.robot.GetContactInfo()
-        if numInvalidContacts > 0:
-            self.penalization_invalid_contact = -10
-        self.power = max(self.power, self.compute_max_power())
-        roll, _, yaw = self.env.robot.GetBaseOrientationRollPitchYaw()
-        _, _, height = self.env.robot.GetBasePosition()
-        self.roll = max(self.roll, abs(roll))
-        self.yaw = max(self.yaw, abs(yaw))
-        self.height = max(self.height, abs(height))
-
-    def get_metric(self):
-        rew_dist = 0.2 * np.exp(-self.compute_forward_distance() ** 2 / 0.1)
-        rew_roll = 0.1 * np.exp(-self.roll**2 / 0.1)
-        rew_yaw = 0.1 * np.exp(-self.yaw**2 / 0.1)
-        max_height = self.height
-        max_power = self.power
-
-        if abs(max_power) >= 0.01:
-            metric = rew_dist + rew_roll + rew_yaw + max_height * 1000 / (2 * max_power)
-        else:
-            metric = 0
-        metric += self.penalization_invalid_contact
-
-        return max(-1, metric)
-
-    def print_metric(self):
-        print("*******************************")
-        print(f"the jump (on place) metric performance amounts to: {self.get_metric():.3f}")
-        print(f"the maximum height peak amounts to: {self.height:.3f}")
-        print("*******************************")
-
-    def step_wait(self):
-
-        if self.flag_first:
-            self.flag_first = False
-            self.init_metric()
-
-        obs, reward, done, infos = self.venv.step_wait()
-        self.eval_metric()
-
-        return obs, reward, done, infos
-
-    def render(self, mode="rgb_array", **kwargs):
-        return self.venv.render(mode, **kwargs)
-
-    def reset(self):
-        obs = self.venv.reset()
-        return obs
-
-    def close(self):
-        self.venv.close()
