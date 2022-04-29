c84014ffae68fa2acca4ac44b9969e006fc4c942
Francesco <f.vezzi@studenti.unipi.it>
2022-04-29 17:40:36 +0200

    added stop_timer

--- quadruped_spring/utils/timer.py
+++ quadruped_spring/utils/timer.py
@@ -4,6 +4,7 @@ import numpy as np
 class Timer:
     def __init__(self):
         self.timer_started = False
+        self.timer_stopped = False
 
     def start_timer(self, timer_time, start_time, delta_time):
         if not self.timer_started:
@@ -11,7 +12,13 @@ class Timer:
             self.timer_time = timer_time
             self.start_time = start_time
             self.end_time = self.start_time + delta_time
+            if not self.timer_stopped:
+                self.relative_timer_time = self.timer_time - self.start_time
             assert self.start_time <= self.end_time, "timer not feasible"
+    
+    def stop_timer(self):
+        self.timer_stopped = True
+        return self.relative_timer_time
 
     def reset_timer(self):
         self.timer_started = False
