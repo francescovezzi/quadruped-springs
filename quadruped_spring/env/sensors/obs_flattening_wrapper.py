5c6b89a183648e434fb1402533c82dc896c16b4e
Francesco <f.vezzi@studenti.unipi.it>
2022-05-02 17:58:32 +0200

    create Sensor objects

---
+++ quadruped_spring/env/sensors/obs_flattening_wrapper.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gym
+
+
+class ObsFlatteningWrapper(gym.Wrapper):
+    """ Wrapper for flattening observation dicitonary. It should be the first wrapper. """
+    
+    def __init__(self, env):
+        super().__init__(env)
+    
+    def step(self, action):
+        observation_dict, reward, done, infos = self.env.step(action)
+        
+        observations = []
+        for key, value in observation_dict.items():
+            observations.append(np.asarray(value).flatten())
+        flat_observations = np.concatenate(observations)
+        
+        return flat_observations, reward, done, infos
+
+    def render(self, mode="rgb_array", **kwargs):
+        return self.env.render(mode, **kwargs)
+
+    def reset(self):
+        obs = self.env.reset()
+        return obs
+
+    def close(self):
+        self.env.close()
---
+++ quadruped_spring/env/sensors/sensor.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class Sensor():
+    """A prototype class for a generic sensor"""
+    def __init__(self):
+        self._data = None
+        self._robot = None
+        self._name = "please give me a name"
+        
+    def _check_dimension(self):
+        assert np.shape(self._high) == np.shape(self._low), "high limit and low limits are different in size"
+        assert np.shape(self._high) == np.shape(self._data), "Observation different from sensor observation limits"
+
+    def _update_sensor_info(self, high, low, noise_std):
+        self._high = high
+        self._low = low
+        self._noise_std = noise_std
+        self._dim = np.shape(self._high)
+
+    def _set_sensor(self, robot):
+        """ Call it at init """
+        self._robot = robot
+        
+    def _reset_sensor(self):
+        """ Call it at reset """
+        pass
+    
+    def _read_data(self):
+        """ Get Sensor data """
+        pass
+    
+    def _on_step(self):
+        """ Callback for step method """
+        pass
+    
+    def _get_data(self):
+        """ Get sensor data """
+        pass
