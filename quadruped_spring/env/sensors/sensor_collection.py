2a98a0de9dfc6dd55687a60537df3c04b10290fe
Francesco <f.vezzi@studenti.unipi.it>
2022-05-05 17:46:35 +0200

    added new sensor equipments

--- quadruped_spring/env/sensors/sensor_collection.py
+++ quadruped_spring/env/sensors/sensor_collection.py
@@ -4,6 +4,10 @@ from quadruped_spring.utils.base_collection import CollectionBase
 # Implemented observation spaces for deep reinforcement learning:
 #   "DEFAULT":  IMU (base linear velocity, base orientation, base angular rate) +
 #               Feet position, Feet velociites, ground reaction force
+#   "CARTESIAN_NO_IMU": Feet position, feet velocities, ground reaction force
+#   "ANGLE_NO_IMU": Joint position, joint velocities, ground reaction force
+#   "CARTESIAN_ANGLE_NO_IMU": CARTESIAN_NO_IMU + ANGLE_NO_IMU
+
 
 
 class SensorCollection(CollectionBase):
@@ -12,5 +16,8 @@ class SensorCollection(CollectionBase):
     def __init__(self):
         super().__init__()
         self._DEFAULT = [rs.IMU, rs.FeetPostion, rs.FeetVelocity, rs.GroundReactionForce]
+        self._CARTESIAN_NO_IMU = [rs.FeetPostion, rs.FeetVelocity, rs.GroundReactionForce]
+        self._ANGLE_NO_IMU = [rs.JointPosition, rs.JointVelocity, rs.GroundReactionForce]
+        self._CARTESIAN_ANGLE_NO_IMU = [rs.FeetPostion, rs.FeetVelocity, rs.JointPosition, rs.JointVelocity, rs.GroundReactionForce]
         self._dict = {"DEFAULT": self._DEFAULT}
         self._element_type = "sensor package"
