c8ed2ddf101e9e836a8196c33dd1d28a7ad6398b
Francesco <f.vezzi@studenti.unipi.it>
2022-05-10 13:00:49 +0200

    fixed load model

--- quadruped_spring/load_model.py
+++ quadruped_spring/load_model.py
@@ -4,6 +4,7 @@ import os
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 os.sys.path.insert(0, currentdir)
 
+from importlib import import_module
 import yaml
 from sb3_contrib import ARS
 from stable_baselines3.common.cmd_util import make_vec_env
@@ -11,11 +12,26 @@ from stable_baselines3.common.vec_env import VecNormalize
 
 from env.quadruped_gym_env import QuadrupedGymEnv
 from quadruped_spring.utils.evaluate_metric import EvaluateMetricJumpOnPlace
+from quadruped_spring.env.wrappers.obs_flattening_wrapper import ObsFlatteningWrapper
+from quadruped_spring.env.wrappers.landing_wrapper import LandingWrapper
 
 LEARNING_ALGS = {"ars": ARS}
 LEARNING_ALG = "ars"
 ENV_ID = "QuadrupedSpring-v0"
-ID = "FirstGood"
+ID = "4"
+
+def callable_env(env_id, wrappers, kwargs):
+    def aux():
+        env = env_id(**kwargs)
+        for wrapper in wrappers:
+            module = ".".join(wrapper.split(".")[:-1])
+            class_name = wrapper.split(".")[-1]
+            module = import_module(module)
+            wrap = getattr(module, class_name)
+            env = wrap(env)
+        env = EvaluateMetricJumpOnPlace(env)
+        return env
+    return aux
 
 # define directories
 aux_dir = "logs/models"
@@ -32,11 +48,10 @@ if os.path.isfile(args_file):
         if loaded_args["env_kwargs"] is not None:
             env_kwargs = loaded_args["env_kwargs"]
 env_kwargs["render"] = True
-if ID == "FirstGood":
-    env_kwargs["adapt_spring_parameters"] = False
+wrapper_list = loaded_args["hyperparams"]['env_wrapper']
 
 # build env
-env = lambda: EvaluateMetricJumpOnPlace(QuadrupedGymEnv(**env_kwargs))
+env = callable_env(QuadrupedGymEnv, wrapper_list, env_kwargs)
 env = make_vec_env(env, n_envs=1)
 env = VecNormalize.load(stats_file, env)
 env.training = False  # do not update stats at test time
@@ -56,8 +71,8 @@ for i in range(sim_steps):
     if dones:
         print(f"episode_reward: {episode_reward}")
         episode_reward = 0
+        break
         # obs = env.reset()
-
 env.env_method("print_metric", indices=0)
 env.close()
 print("end")
