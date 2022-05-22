33d80acd051c7db6eb5625c3a0c7c38866ffccd5
Francesco <f.vezzi@studenti.unipi.it>
2022-05-22 18:28:11 +0200

    changed jumpin dense

--- quadruped_spring/env/tasks/robot_tasks.py
+++ quadruped_spring/env/tasks/robot_tasks.py
@@ -90,27 +90,27 @@ class JumpingInPlaceDense(TaskJumping):
         """Reward calculated each step."""
         lin_vel = self._env.robot.GetBaseLinearVelocity()[[0, 2]]
         _, pitch_rate, _ = self._env.robot.GetTrueBaseRollPitchYawRate()
-        vel_ref = np.array([0.0, 1.0])
+        vel_ref = self._env._robot_sensors.get_desired_velocity()
         track_err = lin_vel - vel_ref
         acc = self._compute_base_acc()
         delta_action = self._compute_delta_action()
 
         pitch_rate_reward = -0.02 * np.abs(pitch_rate)
-        err_reward = 1.0 * np.exp(-np.dot(track_err, track_err) / 0.1)
+        err_reward = 1.0 * np.exp(-np.dot(track_err, track_err) / 0.2**2)
         acc_reward = -0.0005 * np.dot(acc, acc)
         action_reward = -0.00002 * np.dot(delta_action, delta_action)
 
-        reward = pitch_rate_reward + err_reward + acc_reward + action_reward
-
+        # reward = pitch_rate_reward + err_reward + acc_reward + action_reward
+        reward = err_reward
         return reward
 
     def _reward_end_episode(self):
         """Compute bonus and malus to add to reward at the end of the episode"""
         reward = 0
-        if self._terminated():
-            # Malus for crashing
-            # Optionally: no reward in case of crash
-            reward -= 0.04
-        else:
-            reward += 0.04
+        # if self._terminated():
+        #     # Malus for crashing
+        #     # Optionally: no reward in case of crash
+        #     reward -= 0.04
+        # else:
+        #     reward += 0.04
         return reward
