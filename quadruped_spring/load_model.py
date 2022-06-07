1b1c5b295a6d46ea43c19df7f8fd34d79411317b
Francesco <f.vezzi@studenti.unipi.it>
2022-06-07 19:00:23 +0200

    removed not used code + added evaluation on multiple episodes

--- quadruped_spring/load_model.py
+++ quadruped_spring/load_model.py
@@ -8,14 +8,17 @@ from importlib import import_module
 
 import yaml
 from sb3_contrib import ARS
-from stable_baselines3.common.cmd_util import make_vec_env
+from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.utils import set_random_seed
 
 from env.quadruped_gym_env import QuadrupedGymEnv
 from quadruped_spring.env.wrappers.rest_wrapper import RestWrapper
-from quadruped_spring.utils.evaluate_metric import EvaluateMetricJumpOnPlace
 from quadruped_spring.utils.monitor_state import MonitorState
 from quadruped_spring.utils.video_recording import VideoRec
+
+
+SEED = 24
 
 LEARNING_ALGS = {"ars": ARS}
 LEARNING_ALG = "ars"
@@ -23,8 +26,11 @@ ENV_ID = "QuadrupedSpring-v0"
 ID = "1"
 
 REC_VIDEO = False
-SAVE_PLOTS = True
+SAVE_PLOTS = False
 RENDER = False
+EVAL_EPISODES = 10
+ENABLE_ENV_RANDOMIZATION = False
+ENV_RANDOMIZER = "MASS_SETTLING_RANDOMIZER"
 
 
 def callable_env(env_id, wrappers, kwargs):
@@ -44,16 +50,13 @@ def callable_env(env_id, wrappers, kwargs):
             module = import_module(module)
             wrap = getattr(module, class_name)
             env = wrap(env)
-        env = EvaluateMetricJumpOnPlace(env)
         return env
-
     return aux
 
 
 # define directories
 aux_dir = "logs/models"
 model_dir = os.path.join(currentdir, aux_dir, LEARNING_ALG, f"{ENV_ID}_{ID}")
-# model_file = os.path.join(model_dir, "best_model.zip")
 model_file = os.path.join(model_dir, "rl_model_12000000_steps")
 args_file = os.path.join(model_dir, ENV_ID, "args.yml")
 stats_file = os.path.join(model_dir, ENV_ID, "vecnormalize.pkl")
@@ -70,8 +73,8 @@ if RENDER:
 wrapper_list = loaded_args["hyperparams"]["env_wrapper"]
 
 # build env
-env_kwargs["enable_env_randomization"] = False
-env_kwargs["env_randomizer_mode"] = "SETTLING_RANDOMIZER"
+env_kwargs["enable_env_randomization"] = ENABLE_ENV_RANDOMIZATION
+env_kwargs["env_randomizer_mode"] = ENV_RANDOMIZER
 env_kwargs["curriculum_level"] = 0.95
 env = callable_env(QuadrupedGymEnv, wrapper_list, env_kwargs)
 env = make_vec_env(env, n_envs=1)
@@ -88,19 +91,33 @@ custom_objects = {
 model = LEARNING_ALGS[LEARNING_ALG].load(model_file, env, custom_objects=custom_objects)
 print(f"\nLoaded model: {model_file}\n")
 
+set_random_seed(SEED)
 obs = env.reset()
-n_episodes = 1
+n_episodes = EVAL_EPISODES
+total_reward = 0
+total_success = 0
 for _ in range(n_episodes):
     done = False
     while not done:
         action, _states = model.predict(obs, deterministic=True)
         obs, rewards, done, info = env.step(action)
+    total_reward += rewards[0]
+    total_success += info[0]['TimeLimit.truncated']
 
-# env.env_method("print_metric", indices=0)
-# env.env_method("release_video", indices=0)
+avg_reward = total_reward / n_episodes
+avg_success = total_success / n_episodes
+
+if REC_VIDEO:
+    env.env_method("release_video", indices=0)
 if SAVE_PLOTS:
     env.env_method("release_plots", indices=0)
 
+env_randomizer = env.env_method("get_randomizer_mode", indices=0)[0]
+print('\n')
+print(f'over {n_episodes} episodes using {env_randomizer} env randomizer:')
+print(f'average reward -> {avg_reward}' )
+print(f'average success -> {avg_success}')
+print('\n')
 
 env.close()
 print("end")
